ea63847376a248a2e96942aa112ec1d523ea1030
raphaeldpaiva <raphaeldpaiva@gmail.com>
2024-09-17 13:16:50 -0300

    Adds docker-compose aliases

---
+++ aliases/aliases.d/docker-compose.sh
@@ -0,0 +1,5 @@
+alias dcud='docker-compose up -d --build --force-recreate'
+alias dclo='docker-compose logs -f --tail=100'
+alias dcrestart='docker-compose restart'
+alias dcstop='docker-compose stop'
+alias dcstart='docker-compose start'
